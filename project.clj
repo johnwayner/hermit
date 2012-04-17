f4355572e60fef713bfa2bfef3f550616ea5f72d
Wayne Rittimann, Jr <johnwayner@gmail.com>
2012-04-16 21:10:44 -0500

    clojure 1.4

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject hermit "1.0.0-SNAPSHOT"
   :description "DCPU-16 Emulator"
-  :dependencies [[org.clojure/clojure "1.3.0"]
+  :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
-                 [seesaw "1.4.0"]]
+                 [seesaw "1.4.0"]
+                 [overtone/byte-spec "0.3.1"]]
   :main hermit.ui)
